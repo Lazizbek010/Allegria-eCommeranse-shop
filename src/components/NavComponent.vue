5dbdd3929429df4577981ca2ea8f21f917f1bec7
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2023-02-12 18:01:49 +0500

    added similar product

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -21,14 +21,14 @@
                     <li class="nav-text-font">EN</li>
                 </ul>
                 <div class="nav-icons">
-                    <div class="user">
+                    <div class="nav-user">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" fill="none"><path fill="#254A5A" fill-rule="evenodd" d="M14.928 17a7.001 7.001 0 0 0-13.856 0h13.856Zm1.01 0a7.999 7.999 0 0 0-13.595-4.657A8 8 0 0 0 0 18h16a8 8 0 0 0-.063-1ZM8 7a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" clip-rule="evenodd"/></svg>
                     </div>
-                    <div class="heart">
+                    <div class="nav-heart">
                         <svg xmlns="http://www.w3.org/2000/svg" width="21" height="18" fill="none"><path stroke="#254A5A" stroke-linecap="round" d="M2.318 2.318a4.5 4.5 0 0 1 6.364 0L10.5 4.136l1.818-1.818a4.5 4.5 0 0 1 6.364 6.364L10.5 16.864 2.318 8.682a4.5 4.5 0 0 1 0-6.364Z" clip-rule="evenodd"/></svg>
                         <div class="heart-abso description-text">{{ store.cart }}</div>
                     </div>
-                    <div class="cart">
+                    <div class="nav-cart">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="19" fill="none"><path stroke="#254A5A" d="M.5 6.5h15v12H.5zM13 6A5 5 0 0 0 3 6"/></svg>
                     </div>
                 </div>
--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="products__card__item">
         <div class="products__card__item__img">
-            <router-link :to="{name: 'about-product', params: {id: 1}}">
-                <img src="@/assets/images/famous-item1.png" alt=""/>
+            <router-link :to="{ name: 'about-product', params: { id: 1 } }">
+                <img class="products__card__item__img-item" src="@/assets/images/famous-item1.png" alt="" />
             </router-link>
-            <div @click="store.addToCart" class="heart">
-                <svg v-if="store.heart" xmlns="http://www.w3.org/2000/svg" width="21" height="18" fill="none">
-                    <path stroke="#254A5A" stroke-linecap="round"
-                        d="M2.318 2.318a4.5 4.5 0 0 1 6.364 0L10.5 4.136l1.818-1.818a4.5 4.5 0 0 1 6.364 6.364L10.5 16.864 2.318 8.682a4.5 4.5 0 0 1 0-6.364Z"
-                        clip-rule="evenodd" />
-                </svg>
-                <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="17" fill="none">
-                    <path stroke="#E64926" stroke-linecap="round" stroke-width="9" d="m5 5 5 5 5-5" />
-                </svg>
-            </div>
+            <heart-component></heart-component>
             <div class="discount">
                 <span class="description-text">40 %</span>
             </div>
@@ -25,7 +16,7 @@
 </template>
 
 <script setup>
-import { useCounterStore } from '@/stores/Counter.js';
-const store = useCounterStore();
+import HeartComponent from '@/components/HeartComponent.vue';
+
 </script>
 
