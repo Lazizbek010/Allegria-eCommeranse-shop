8ac1b29835eaa922ea3da76bbc74cd8449ec361b
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2023-02-15 11:35:16 +0500

    media brands

--- src/views/pages/BrandsView.vue
+++ src/views/pages/BrandsView.vue
@@ -14,26 +14,30 @@
             </div>
         </section>
         <section class="american-vintage">
-            <div class="american-vintage__info">
-                <div class="american-vintage__info__left">
-                    <img src="@/assets/images/about-us-info-img2.png" alt="">
-                </div>
-                <div class="american-vintage__info__right">
-                    <h1 class="avenir-800">American vintage</h1>
-                    <p class="description-text">Продукция также включает немногочисленные аксессуары в виде шарфов,
-                        ремней, шапок, а также
-                        женских сумок.
-                        Несмотря на название «American Vintage», бренд создан французом Мишель Азулэ.
-                        Отличие бренда от других - это простота в исполнении изделий (минимализм) и 100% натуральные
-                        ткани. бренд сочетает в себе расслабленность южной Франции и простоту Америки. При создании
-                        одежды используется очень тонкий высококачественный трикотаж, который дает ощущение «второй
-                        кожи». Основная цветовая гамма - это спокойные пастельные расцветки: ореховые, бежевые, желтые,
-                        серые и так далее.
-                        На первый взгляд, кажется, что вещи просты в своем исполнении, но вся продукция от этой марки
-                        выглядит очень стильно, оставляя без ума многих покупателей.
+            <div class="container">
+                <div class="american-vintage__info">
+                    <div class="american-vintage__info__left">
+                        <img src="@/assets/images/about-us-info-img2.png" alt="">
+                    </div>
+                    <div class="american-vintage__info__right">
+                        <h1 class="avenir-800">American vintage</h1>
+                        <p class="description-text">Продукция также включает немногочисленные аксессуары в виде шарфов,
+                            ремней, шапок, а также
+                            женских сумок.
+                            Несмотря на название «American Vintage», бренд создан французом Мишель Азулэ.
+                            Отличие бренда от других - это простота в исполнении изделий (минимализм) и 100% натуральные
+                            ткани. бренд сочетает в себе расслабленность южной Франции и простоту Америки. При создании
+                            одежды используется очень тонкий высококачественный трикотаж, который дает ощущение «второй
+                            кожи». Основная цветовая гамма - это спокойные пастельные расцветки: ореховые, бежевые,
+                            желтые,
+                            серые и так далее.
+                            На первый взгляд, кажется, что вещи просты в своем исполнении, но вся продукция от этой
+                            марки
+                            выглядит очень стильно, оставляя без ума многих покупателей.
 
-                    </p>
-                    <img src="@/assets/images/about-us-info-img2.png" alt="">
+                        </p>
+                        <img src="@/assets/images/about-us-info-img2.png" alt="">
+                    </div>
                 </div>
             </div>
         </section>
@@ -45,31 +49,38 @@
                         лидером на итальянском рынке спортивной одежды премиум-класса. </p>
                 </div>
             </div>
-            <div class="deha__about">
-                <div class="deha__about__left">
-                    <h1 class="avenir-800">DeHA</h1>
-                    <p class="description-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi esse sed
-                        totam incidunt! Nisi eveniet soluta sed laudantium perferendis voluptate hic molestias, id eos
-                        expedita consequatur doloremque illum, iusto nulla quidem impedit alias voluptatum aliquam, non
-                        vitae tenetur voluptatem reiciendis pariatur aspernatur. Quod esse corporis molestiae quis
-                        facere quia alias, suscipit velit amet rem omnis sit eius repudiandae ex placeat iste ipsum non
-                        cumque, nostrum autem nulla qui quos error. Totam reprehenderit soluta dolore excepturi mollitia
-                        corporis est, itaque harum?
-                    </p>
-                    <div class="deha__about__left__img">
-                        <img src="@/assets/images/deha-about-img01.png" alt="">
-                        <div class="deha__about__left__img__shape">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="273" height="299" fill="none">
-                                <path fill="#CEC6C4" stroke="#CEC6C4" stroke-width=".393"
-                                    d="M272.018 232.636.432 66.423l.205-.335L272.223 232.3z" />
-                                <path stroke="#CEC6C4"
-                                    d="M102.218 75.8a61.499 61.499 0 0 0-24.01 116.72 61.51 61.51 0 0 0 31.534 6.038 61.502 61.502 0 0 0 51.051-35.156" />
-                            </svg>
+            <div class="container">
+                <div class="deha__about">
+                    <div class="deha__about__left">
+                        <h1 class="avenir-800">DeHA</h1>
+                        <p class="description-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi esse
+                            sed
+                            totam incidunt! Nisi eveniet soluta sed laudantium perferendis voluptate hic molestias, id
+                            eos
+                            expedita consequatur doloremque illum, iusto nulla quidem impedit alias voluptatum aliquam,
+                            non
+                            vitae tenetur voluptatem reiciendis pariatur aspernatur. Quod esse corporis molestiae quis
+                            facere quia alias, suscipit velit amet rem omnis sit eius repudiandae ex placeat iste ipsum
+                            non
+                            cumque, nostrum autem nulla qui quos error. Totam reprehenderit soluta dolore excepturi
+                            mollitia
+                            corporis est, itaque harum?
+                        </p>
+                        <div class="deha__about__left__img">
+                            <img src="@/assets/images/deha-about-img01.png" alt="">
+                            <div class="deha__about__left__img__shape">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="273" height="299" fill="none">
+                                    <path fill="#CEC6C4" stroke="#CEC6C4" stroke-width=".393"
+                                        d="M272.018 232.636.432 66.423l.205-.335L272.223 232.3z" />
+                                    <path stroke="#CEC6C4"
+                                        d="M102.218 75.8a61.499 61.499 0 0 0-24.01 116.72 61.51 61.51 0 0 0 31.534 6.038 61.502 61.502 0 0 0 51.051-35.156" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="deha__about__right">
-                    <img src="@/assets/images/deha-about-img01.png" alt="">
+                    <div class="deha__about__right">
+                        <img src="@/assets/images/deha-about-img01.png" alt="">
+                    </div>
                 </div>
             </div>
         </section>
@@ -82,7 +93,8 @@
                     имела свое имя. </p>
             </div>
             <div class="lucy__img"></div>
-            <div class="lucy__about">
+            <div class="container">
+                <div class="lucy__about">
                 <div class="lucy__about__img">
                     <img src="@/assets/images/about-us-info-img1.png" alt="">
                 </div>
@@ -99,15 +111,17 @@
                     </p>
                 </div>
             </div>
+            </div>
         </section>
         <section class="birkenstock">
             <div class="birkenstock__img">
-                <div class="birkenstock__img__info">
+                <div class="birkenstock__img__info container">
                     <h1 class="cormorant-500"><span>бренд</span>birkenstock</h1>
                     <p class="description-text">BIRKENSTOCK — известная немецкая компания, занимающаяся производством ортопедической обуви.</p>
                 </div>
             </div>
-            <div class="birkenstock__about">
+            <div class="container">
+                <div class="birkenstock__about">
                 <div class="birkenstock__about__left">
                     <h1 class="avenir-800">birkenstock</h1>
                     <p class="description-text">Свою славу она заслужила благодаря удобству, экологичности материалов и поддержанию здоровья ног.
@@ -123,13 +137,14 @@
                     <div class="birkenstock__about__left__img">
                         <img src="@/assets/images/bg-hero-four1.png" alt="">
                         <div class="birkenstock__about__left__img-shape">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="273" height="299" fill="none"><path fill="#CEC6C4" stroke="#CEC6C4" stroke-width=".393" d="M272.018 232.636.432 66.423l.205-.335L272.223 232.3z"/><path stroke="#CEC6C4" d="M102.218 75.8a61.499 61.499 0 0 0-24.01 116.72 61.51 61.51 0 0 0 31.534 6.038 61.502 61.502 0 0 0 51.051-35.156"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="240" height="270" fill="none"><path fill="#CEC6C4" stroke="#CEC6C4" stroke-width=".393" d="M272.018 232.636.432 66.423l.205-.335L272.223 232.3z"/><path stroke="#CEC6C4" d="M102.218 75.8a61.499 61.499 0 0 0-24.01 116.72 61.51 61.51 0 0 0 31.534 6.038 61.502 61.502 0 0 0 51.051-35.156"/></svg>
                         </div>
                     </div>
                 </div>
                 <div class="birkenstock__about__right">
                     <img src="@/assets/images/bg-hero-four2.png" alt="">
                 </div>
+            </div>
             </div>
         </section>
     </div>
