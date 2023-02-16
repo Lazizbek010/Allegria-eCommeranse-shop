70711f075f2a2d6b43f8e5eb5c6b8d763a93bb5c
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2023-02-16 23:30:41 +0500

    modals setting

--- src/views/pages/AboutProductView.vue
+++ src/views/pages/AboutProductView.vue
@@ -22,7 +22,7 @@
                     <h1 class="about-product__name avenir-800">American vintage</h1>
                     <h6 class="about-product__title">Classic shirt</h6>
                     <p class="about-product__price">
-                        <span class="product-old-item">6100 UAH</span> 
+                        <span class="product-old-item">6100 UAH</span>
                         <span class="product-new-item">3800 UAH</span>
                     </p>
                     <div class="about-product-slider"></div>
@@ -48,8 +48,8 @@
                         <button class="btn buy-btn">купить в один клик</button>
                     </div>
                     <div class="selection2">
-                        <button @click="openModal" class="select-font">Оплата и доставка</button>
-                        <button @click="openModalReturn" class="select-font">Возврат и обмен</button>
+                        <button @click="openPaymentModal = true" class="select-font">Оплата и доставка</button>
+                        <button @click="openReturnModal = true" class="select-font">Возврат и обмен</button>
                     </div>
                     <h4 class="about-product__desc-500">ИНФОРМАЦИЯ О ТОВАРЕ</h4>
                     <p class="about-product__desc-title select-font">Lorem ipsum dolor sit amet.</p>
@@ -59,69 +59,56 @@
         <div class="about-product__container">
             <swiper class="similar-products" :slides-per-view="3" :space-between="15">
                 <swiper-slide v-for="sim in 9" :key="sim">
-                    <product-component ></product-component>
+                    <product-component></product-component>
                 </swiper-slide>
-                <!-- <div class="left">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="41" fill="none">
-                        <path stroke="#254A5A" d="M17 1 1 20.5 17 40" />
-                    </svg>
-                </div>
-                <div class="right">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="41" fill="none">
-                        <path stroke="#254A5A" d="m1 1 16 19.5L1 40" />
-                    </svg>
-                </div> -->
             </swiper>
         </div>
         <!-- Payment Modal -->
-        <div class="overlay" v-show="openPaymentModal" @click="closeModal"></div>
-        <div class="modal" v-show="openPaymentModal">
-            <div class="modal__payment" :class="{ 'showModal': openPaymentModal }">
-                <close-btn-component @click="closeModal"></close-btn-component>
-                <h3 class="products__menu__item uppercase">Оплата</h3>
-                <h3 class="products__menu__item">Наличными при получении</h3>
-                <p class="description-text">Оплата осуществляется наличными денежными средствами курьеру
-                    непосредственно при доставке заказа на адрес получателя. Выбрав данный способ оплаты, Вы
-                    получаете возможность рассмотреть и примерить заказанный товар. <br> <br> Если какая-то позиция
-                    Вам не подошла, Вы можете просто вернуть ее курьеру, не оплачивая.</p>
-                <h3 class="products__menu__item">Картой на сайте (Visa, Mastercard)</h3>
-                <p class="description-text">На сайте нашего интернет-магазина мы принимаем оплату платежными картами
-                    Visa и Mastercard. Безопасность проведения платежей у нас гарантирована системой
-                    eCommerceConnect с использованием современного стандарта «3-D Secure».</p>
-                <h3 class="products__menu__item uppercase">Доставка</h3>
-                <h3 class="products__menu__item">На отделение «Нова Пошта»</h3>
-                <p class="description-text">Доставка заказов клиентам интернет-магазина ALLEGRIA осуществляется по
-                    территории всей Украины курьерской службой "Нова пошта".</p>
-            </div>
+        <div class="overlay" v-show="openPaymentModal" @click="openPaymentModal = false"></div>
+        <div v-show="openPaymentModal" class="modal modal__payment" :class="{ 'showModal': openPaymentModal }">
+            <close-btn-component @click="openPaymentModal = false"></close-btn-component>
+            <h3 class="products__menu__item uppercase">Оплата</h3>
+            <h3 class="products__menu__item">Наличными при получении</h3>
+            <p class="description-text">Оплата осуществляется наличными денежными средствами курьеру
+                непосредственно при доставке заказа на адрес получателя. Выбрав данный способ оплаты, Вы
+                получаете возможность рассмотреть и примерить заказанный товар. <br> <br> Если какая-то позиция
+                Вам не подошла, Вы можете просто вернуть ее курьеру, не оплачивая.</p>
+            <h3 class="products__menu__item">Картой на сайте (Visa, Mastercard)</h3>
+            <p class="description-text">На сайте нашего интернет-магазина мы принимаем оплату платежными картами
+                Visa и Mastercard. Безопасность проведения платежей у нас гарантирована системой
+                eCommerceConnect с использованием современного стандарта «3-D Secure».</p>
+            <h3 class="products__menu__item uppercase">Доставка</h3>
+            <h3 class="products__menu__item">На отделение «Нова Пошта»</h3>
+            <p class="description-text">Доставка заказов клиентам интернет-магазина ALLEGRIA осуществляется по
+                территории всей Украины курьерской службой "Нова пошта".</p>
         </div>
         <!-- Return and exchange Modal -->
-        <div class="overlay" v-show="openReturnModal" @click="closeModalReturn"></div>
-        <div class="modal" v-show="openReturnModal">
-            <div class="modal__payment modal__return" :class="{ 'showModal': openReturnModal }">
-                <close-btn-component @click="closeModalReturn"></close-btn-component>
-                <h3 class="products__menu__item uppercase modal__return-name">Возврат и обмен</h3>
-                <p class="description-text">Возврат товаров, приобретенных в интернет-магазине ALLEGRIA, происходит
-                    согласно Закону Украины «О защите прав потребителей».</p>
-                <p class="description-text">Вы можете вернуть товар на протяжении 14 дней со дня покупки.</p>
-                <h3 class="products__menu__item">Как оформить заявку на возврат?</h3>
-                <p class="description-text">Если купленная вещь Вам не понравилась или не подошла — свяжитесь с нашим
-                    контакт центром по телефону 000 00 000 и мы поможем оформить заявку на возврат.</p>
-                <h3 class="products__menu__item">Какие есть условия по возврату товара?</h3>
-                <p class="description-text">Обмен и возврат товара производится в том случае, если указанный товар не
-                    был в употреблении, полностью сохранен его товарный вид — без повреждений и следов ношения,
-                    оригинальная упаковка, а также бирки, пломбы, ярлыки, фирменные знаки.</p>
-                <p class="description-text">Не возвращаются и не подлежат обмену купальники, нижнее белье,
-                    чулочно-носочные изделия, предметы личной гигиены, перчатки.</p>
-                <p class="description-text">Вы можете осуществить возврат товар непосредственно в наших розничных
-                    магазинах, расположенных в Киеве, Харькове, Днепре и Одессе. Для жителей других городов Украины
-                    пересылка товара перевозчиком Новой Почтой или любым другим, на склад интернет магазина ALLEGRIA,
-                    расположенного в городе Харькове, осуществляется за счет компании.</p>
-            </div>
+        <div class="overlay" v-show="openReturnModal" @click="openReturnModal = false"></div>
+        <div v-show="openReturnModal" class="modal__payment modal__return modal" :class="{ 'showModal': openReturnModal }">
+            <close-btn-component @click="openReturnModal = false"></close-btn-component>
+            <h3 class="products__menu__item uppercase modal__return-name">Возврат и обмен</h3>
+            <p class="description-text">Возврат товаров, приобретенных в интернет-магазине ALLEGRIA, происходит
+                согласно Закону Украины «О защите прав потребителей».</p>
+            <p class="description-text">Вы можете вернуть товар на протяжении 14 дней со дня покупки.</p>
+            <h3 class="products__menu__item">Как оформить заявку на возврат?</h3>
+            <p class="description-text">Если купленная вещь Вам не понравилась или не подошла — свяжитесь с нашим
+                контакт центром по телефону 000 00 000 и мы поможем оформить заявку на возврат.</p>
+            <h3 class="products__menu__item">Какие есть условия по возврату товара?</h3>
+            <p class="description-text">Обмен и возврат товара производится в том случае, если указанный товар не
+                был в употреблении, полностью сохранен его товарный вид — без повреждений и следов ношения,
+                оригинальная упаковка, а также бирки, пломбы, ярлыки, фирменные знаки.</p>
+            <p class="description-text">Не возвращаются и не подлежат обмену купальники, нижнее белье,
+                чулочно-носочные изделия, предметы личной гигиены, перчатки.</p>
+            <p class="description-text">Вы можете осуществить возврат товар непосредственно в наших розничных
+                магазинах, расположенных в Киеве, Харькове, Днепре и Одессе. Для жителей других городов Украины
+                пересылка товара перевозчиком Новой Почтой или любым другим, на склад интернет магазина ALLEGRIA,
+                расположенного в городе Харькове, осуществляется за счет компании.</p>
         </div>
-    </div>
+</div>
 </template>
 
 <script setup>
+
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import ProductComponent from '@/components/ProductComponent.vue';
@@ -130,31 +117,12 @@ import CloseBtnComponent from '../../components/CloseBtnComponent.vue';
 import { ref } from 'vue';
 const openPaymentModal = ref(false)
 const openReturnModal = ref(false)
-//  Payment Modal
-function openModal() {
-    openPaymentModal.value = true;
-}
-function closeModal() {
-    openPaymentModal.value = false;
-}
-// Return Modal
-function openModalReturn() {
-    openReturnModal.value = true;
-}
-
-function closeModalReturn() {
-    openReturnModal.value = false;
-}
 //  DOCUMENT ADDEVENTLISTENER 1
-document.addEventListener('keydown', (e)=>{
-    if(e.key === 'Escape'){
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
         openPaymentModal.value = false;
         openReturnModal.value = false;
     }
 })
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
